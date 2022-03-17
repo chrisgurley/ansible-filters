5d8c93bb2dcf0efed7787eb2faa79da9d3913bf5
Christopher Gurley <chris@gurleyit.com>
2022-03-16 21:53:27 -0500

    🎉Initialize repo with first filter

---
+++ filter_plugins/get_ranges.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'get_ranges': self.get_ranges,
+        }
+
+    def get_ranges(self, data, safe: bool = False, validate_input: bool = True, remove_unwanted: bool = False):
+        try:
+            if safe:
+                if len(data) != len([x for x in data if type(x) == int]):
+                    raise ValueError('Received list that contained items that were not of type `int`')
+
+            if validate_input:
+                if type(data) != list:
+                    raise TypeError(f'Invalid type received, expected list, got {type(data)})')
+
+                if len(data) == 0:
+                    raise ValueError('Received list that did not contain any data')
+
+            if remove_unwanted:
+                print('WARNING: `remove_unwanted` set to True, any item in the given list will be removed')
+                original_size = len(data)
+                data = [x for x in data if type(x) == int]
+                print(f'Removed {original_size-len(data)} items from list')
+
+        except Exception as e:
+            print(f'fatal: {e}')
+            exit(128)
+
+        # Ensure we only have unique values and resort
+        data = sorted(list(set(data)))
+        worked_list = []
+
+        while len(data) > 0:
+            working_list = []
+            for i in range(len(data)):
+                if (data[0] + i) == data[0+i]:
+                    working_list.append(data[i])
+                else:
+                    break
+            data = sorted(list(set(data) - set(working_list)))
+            if len(working_list) == 1:
+                worked_list.append(f'{working_list[0]}')
+            else:
+                worked_list.append(f'{working_list[0]}-{working_list[-1]}')
+        return worked_list
